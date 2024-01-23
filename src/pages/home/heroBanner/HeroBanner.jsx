baf94c3686235a494df401d4ef3c04783407dbad
Abhishek313 <vabhi5149@gmail.com>
2024-01-23 21:55:24 +0530

    handled search button

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -28,6 +28,12 @@ const HeroBanner = () => {
         }
     };
 
+    const searchQueryHandlerByButton = (event) => {
+        if ( query.length > 0) {
+            navigate(`/search/${query}`);
+        }
+    };
+
     return (
         <div className="heroBanner">
             {!loading && (
@@ -51,7 +57,7 @@ const HeroBanner = () => {
                             onChange={(e) => setQuery(e.target.value)}
                             onKeyUp={searchQueryHandler}
                         />
-                        <button>Search</button>
+                        <button onClick={searchQueryHandlerByButton}>Search</button>
                     </div>
                 </div>
             </ContentWrapper>
