5926ea036069291ee937068ca41c57ff665ff466
Abhishek Prasad <vabhi5149@gmail.com>
2024-01-29 14:55:52 +0530

    Update HeroBanner.jsx

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -53,7 +53,7 @@ const HeroBanner = () => {
                     <div className="searchInput">
                         <input
                             type="text"
-                            placeholder="Search for a movie or tv show...."
+                            placeholder="Search for a movie or tv show.... eg: Titanic "
                             onChange={(e) => setQuery(e.target.value)}
                             onKeyUp={searchQueryHandler}
                         />
